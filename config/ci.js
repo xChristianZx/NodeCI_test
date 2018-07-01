e9831a45389eb7510e8644deb8b0f53aaeed00c7
Christian Zenaty <cazenaty@yahoo.com>
2018-07-01 11:58:08 -0700

    Initial commit and travis config

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "729737724086-q0ecvks515adt0nttoce3fdnmtgqc3no.apps.googleusercontent.com",
   googleClientSecret: "g80zN0-mvNDRNPVCitaxwWZj",
-  mongoURI: "mongodb://christian:passw0rd@ds018498.mlab.com:18498/blog_dev",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379"
 };
